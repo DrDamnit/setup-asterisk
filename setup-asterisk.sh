504b41a35b771c84a5ac48f783d277bc7cc8f5eb
root <root@l3voip>
2015-06-25 12:31:08 -0400

    Initial import

--- setup-asterisk.sh
+++ setup-asterisk.sh
@@ -5,6 +5,7 @@
 #2. Extract them to asterisk/, libpri/, and dahdi/, respectively.
 #3. Run this script, and enjoy.
 #4. PS. If you want to install LAMP as well, include the --lamp parameter
+#If you want to install asterisk as a non-root user, use the --update parameter. (Reference: http://asteriskdocs.org/en/2nd_Edition/asterisk-book-html-chunk/asterisk-CHP-13-SECT-4.html)
 
 echo -ne Checking for asterisk source...
 if [ -d /usr/src/asterisk ]
@@ -59,7 +60,7 @@ apt-get install linux-headers-`uname -r`
 
 #Install other needed stuff
 
-aptitude install libconfig-tiny-perl libcupsimage2 libcups2 libmime-lite-perl libemail-date-format-perl libfile-sync-perl libfreetype6 libspandsp1 libtiff-tools libtiff4 libjpeg62 libmime-types-perl libpaper-utils psutils libpaper1 ncurses ncurses-dev libncurses-dev libncurses-gst ncurses-term libnewt libnewt-dev libnewt-pic libxml2 libxml2-dev libspandsp-dev libspandsp1
+aptitude install libconfig-tiny-perl libcupsimage2 libcups2 libmime-lite-perl libemail-date-format-perl libfile-sync-perl libfreetype6 libspandsp1 libtiff-tools libtiff4 libjpeg62 libmime-types-perl libpaper-utils psutils libpaper1 ncurses ncurses-dev libncurses-dev libncurses-gst ncurses-term libnewt libnewt-dev libnewt-pic libxml2 libxml2-dev libspandsp-dev libspandsp1 pwgen
 
 #Install LAMP if asked to...
 if [ "$1" = "--lamp" ];
@@ -113,6 +114,7 @@ function compile_asterisk() {
 	cd /usr/src/asterisk
 	echo "Compiling asterisk."
 	read -p "Press [ENTER] to continue..."
+
 	./configure
 	make
 	make install
@@ -120,6 +122,54 @@ function compile_asterisk() {
 	make config
 	make samples
 }
+
+setupUser() {
+     PASS=`pwgen -cns 32 1`
+     echo "Setting up user: $1"
+     useradd -m $1
+     usermod -s /bin/bash $1
+ 
+     echo "Setting password to: $PASS"
+     echo "$1:$PASS" | chpasswd
+ 
+     echo "Saving information to setup.log"
+     echo $1 : $PASS >> setup.log
+ }
+
+#Assume we are not upgrading anything.
+if [ "$1" == "--nonroot" ]
+then
+	echo -n "Checking to see if asterisk user exists..."
+	USEREXISTS=`grep -rin asterisk /etc/passwd`
+	if [ ${USEREXISTS} ]; then
+		echo "YES"
+	else
+		echo "NO"
+		echo "Setting up user..."
+		setupUser asterisk
+	fi
+	compile_libpri
+	compile_dahdhi_kernel
+	compile_dahdi_tools
+	echo "Everything but Asterisk has been compiled. Now, you need to create the non-root user ('asterisk'?), and compile using the following configure script:"
+	echo ""
+	echo "su asterisk"
+	echo "cd /usr/src/asterisk"
+	echo "make clean"
+	echo "./configure --prefix=$HOME/asterisk-bin --sysconfdir=$HOME/asterisk-bin --localstatedir=$HOME/asterisk-bin"
+	echo "make menuselect"
+	echo "make"
+	echo "make install"
+	echo ""
+	# echo "Note: for reference, here's how to create the user:"
+	# echo ""
+	# echo 'adduser -c "Asterisk PBX" asterisk'
+	# echo "passwd asterisk"
+	# echo ""
+	# echo "SET A STRONG PASSWORD FOR THE ASTERISK USER!"
+	# echo "Use the pwgen utility to generate a 32 character random password to effectively disable it, then enable key authentication to manage the asterisk user"
+	exit;
+fi
 
 #Assume we are not upgrading anything.
 if [ "$1" != "--update" ]
@@ -169,6 +219,3 @@ if [ "$1" == "--update" ]
 	fi
 echo "Recompiles / upgrades completed. You will need to restart services for the new version to take effect."
 fi
-
-
-	
