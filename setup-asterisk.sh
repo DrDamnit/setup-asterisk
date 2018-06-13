52b9a1a4125af9e24696f31818d10610b2ca2969
michael <mj@hph.io>
2018-06-13 19:59:52 -0400

    Full paths added to functions.

--- setup-asterisk.sh
+++ setup-asterisk.sh
@@ -146,12 +146,12 @@ EOF
 
 function clean_module() {
     echo "Cleaning: $1"
-    BUFFER=`locate $1`
+    BUFFER=`/usr/bin/locate $1`
     echo ${BUFFER}
     for PATH in ${BUFFER[@]}
     do
         echo "Removing: ${PATH}"
-        rm -v ${PATH}
+        /bin/rm -v ${PATH}
     done
 }
 
